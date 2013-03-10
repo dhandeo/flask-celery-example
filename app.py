e890d5d35347d4886f27afd388b1d86536657560
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-03-09 23:35:32 -0500

    Able to start a long running process, and monitor the progress through flask

--- app.py
+++ app.py
@@ -2,29 +2,30 @@ from os import path, environ
 import json
 from flask import Flask, Blueprint, abort, jsonify, request, session
 import settings
-from celery import Celery
+from celery import Celery, current_task
 from tasks import add
+from time import sleep
+from flask import render_template
 
 app = Flask(__name__)
 app.config.from_object(settings)
 
-@app.route("/test")
+@app.route("/")
 def hello_world(x=16, y=16):
     x = int(request.args.get("x", x))
     y = int(request.args.get("y", y))
     res = add.apply_async((x, y))
     context = {"id": res.task_id, "x": x, "y": y}
-    result = "add((x){}, (y){})".format(context['x'], context['y'])
+    task = "wait for %d seconds and then return %d" % (x, y)
     goto = "{}".format(context['id'])
-    return jsonify(result= result, goto=goto) 
+    return render_template("task.html", task=task, goto="http://127.0.0.1:5000/result/", task_id=str(context['id']))
 
-@app.route("/test/result/<task_id>")
+@app.route("/result/<task_id>")
 def show_result(task_id):
-    retval = add.AsyncResult(task_id).get(timeout=1.0)
-    return repr(retval)
-
-print app.url_map
+    ret = add.AsyncResult(task_id)
+    return render_template("result.html", result=ret)
 
 if __name__ == "__main__":
+    print app.url_map
     port = int(environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
--- tasks.py
+++ tasks.py
@@ -1,12 +1,15 @@
-from celery import Celery
+from celery import Celery, current_task
+from time import sleep
 
-celery = Celery("tasks",
-                broker='redis://localhost:6379/0',
-                backend='redis')
+celery = Celery(broker="mongodb://127.0.0.1/celery", backend="mongodb://127.0.0.1/celery")
 
 @celery.task(name="tasks.add")
 def add(x, y):
-    return x + y
+    print "Starting job ..", x, y
+    for i in range(x):
+        sleep(1)
+        current_task.update_state(state='PROGRESS', meta=str({'current': i, 'total': x}))
+    return y
 
 if __name__ == "__main__":
     celery.start()
